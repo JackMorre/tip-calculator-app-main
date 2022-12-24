cc8fb68dcde9c8d015c7dce087fee3298ba95325
Jack Morrell <jack.morrell79@googlemail.com>
2022-12-25 00:51:08 +0100

    update for cleaner code

--- script.js
+++ script.js
@@ -1,59 +1,76 @@
-// get all elements
-
-//create function to be used with event listener
-
-// make it so that nothing happens until all three fields are field up
-
-// make is so either you have a pre selected percentage or customer
-
 const reset = document.querySelector(".reset");
-
-let tipAmount = document.querySelector(".tip-price");
+const tipAmount = document.querySelector(".tip-price");
 let total = document.querySelector(".total");
-
 const bill = document.querySelector(".bill");
 const people = document.querySelector(".number-people");
-
 const radioBtn = document.querySelectorAll('input[type="radio"]');
-let percentCustom = document.querySelector(".custom");
+const percentCustom = document.querySelector(".custom");
 
 let selectedValue = "";
 let selectedElement = "";
-letpeopleValue = "";
+let peopleValue = "";
 
-const checkEverything = () => {
-	if (percentCustom.value > 0) {
-		selectedValue = percentCustom.value;
-	}
-	if (people.value == 0) {
-		people.parentElement.classList.toggle("no-input");
-	}
-	peopleValue = Number(people.value);
-	if (bill.value > 0 && people.value > 0 && selectedValue > 0) {
-		//create tip for bill
-		const fullTip = (bill.value / 100) * selectedValue;
-		const personTip = fullTip / peopleValue;
-		console.log(bill.value, people.value, selectedValue);
-		const personBill = bill.value / peopleValue;
-		const personTotal = personBill + personTip;
-		const pTipTwoDec = (Math.round(personTip * 100) / 100).toFixed(2);
-		const pTTwoDec = (Math.round(personTotal * 100) / 100).toFixed(2);
-		tipAmount.textContent = pTipTwoDec;
-		total.textContent = pTTwoDec;
+const peopleCheck = () => {
+	if (people.value == 0 || people.value == "") {
+		people.parentElement.classList.add("no-input");
+	} else if (people.value > 0) {
+		people.parentElement.classList.remove("no-input");
+		peopleValue = Number(people.value);
 	}
 };
 
+const customValue = () => {
+	if (percentCustom.value > 0) {
+		selectedValue = percentCustom.value;
+	}
+};
+
+const checkEverything = () => {
+	const fullTip = (bill.value / 100) * selectedValue;
+	const personTip = fullTip / peopleValue;
+	const personBill = bill.value / peopleValue;
+	const personTotal = personBill + personTip;
+	const pTipTwoDec = (Math.round(personTip * 100) / 100).toFixed(2);
+	const pTTwoDec = (Math.round(personTotal * 100) / 100).toFixed(2);
+	tipAmount.textContent = pTipTwoDec;
+	total.textContent = pTTwoDec;
+};
+
+const allChecked = () => {
+	if (bill.value > 0 && people.value > 0 && selectedValue > 0) {
+		checkEverything();
+	}
+};
+
+const resetfunction = () => {
+	tipAmount.textContent = `0.00`;
+	total.textContent = `0.00`;
+	people.value = "";
+	percentCustom.value = "";
+	selectedElement.checked = false;
+	bill.value = "";
+};
+
 bill.addEventListener("input", () => {
-	console.log("hello");
-	setInterval(checkEverything(), 1000);
+	allChecked();
 });
+
 people.addEventListener("input", () => {
-	setInterval(checkEverything(), 100);
+	peopleCheck();
+	allChecked();
+});
+
+people.addEventListener("blur", () => {
+	peopleCheck();
+});
+
+percentCustom.addEventListener("click", () => {
+	selectedElement.checked = false;
 });
 
 percentCustom.addEventListener("input", () => {
-	selectedElement.checked = false;
-	setInterval(checkEverything(), 100);
+	customValue();
+	allChecked();
 });
 
 radioBtn.forEach((el) => {
@@ -61,17 +78,10 @@ radioBtn.forEach((el) => {
 		if (el.checked === true) {
 			selectedElement = el;
 			selectedValue = el.value;
-			console.log(selectedElement);
 			percentCustom.value = "";
 		}
-		setInterval(checkEverything(), 100);
+		allChecked();
 	});
 });
-reset.addEventListener("click", () => {
-	tipAmount.textContent = `0.00`;
-	total.textContent = `0.00`;
-	people.value = "";
-	percentCustom.value = "";
-	selectedElement.checked = false;
-	bill.value = "";
-});
+
+reset.addEventListener("click", resetfunction);
